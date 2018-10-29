fbebf2ad43281448a5c469746ac4767422dc36c7
sven <1070700506@qq.com>
2018-10-29 02:31:19 +0000

    show user profile and edit

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@
 */
 
 Route::get('/', 'PagesController@root')->name('root');
-Auth::routes();
 
 
+//Auth::routes();
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
@@ -29,3 +29,5 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+Route::resource('users','UsersController',['only'=>['edit','update','show']]);
