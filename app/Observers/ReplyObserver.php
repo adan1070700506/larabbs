861cb70603fe8d2c67ed21775770b106eb646289
vagrant <vagrant@homestead>
2018-11-04 16:25:00 +0000

    add reply notifications

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,20 +3,22 @@
 namespace App\Observers;
 
 use App\Models\Reply;
-
+use App\Notifications\TopicReplied;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class ReplyObserver
 {
 
-    public function creating (){
+    public function creating (Reply $reply){
         $reply->content = clean($reply->content, 'user_topic_body');
     }
     public function created(Reply $reply)
     {
         //
         $reply->topic->increment('reply_count',1);
+
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
 
